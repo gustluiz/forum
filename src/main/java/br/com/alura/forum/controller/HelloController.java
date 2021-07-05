5cf0f0ab9d8589f5ddc31cc38784d387e97ffbf3
Gustavo L. Ferreira <gustavo.l.ferreira@gmail.com>
2021-07-05 20:23:14 -0300

    Adding src

---
+++ src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -0,0 +1,21 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	int counter = 0;
+	@RequestMapping("/")
+	@ResponseBody
+	public String helloWorld() {
+		
+		
+		
+		System.out.println("Passei pelo controller " + counter++);
+		return "Hello World!";
+		
+	}
+}
